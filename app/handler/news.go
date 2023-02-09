ac2ac975c22be0fc3694b98c9f9ca5966fcd2ca6
PuzenkoYev <yevhenpuzenko@gmail.com>
2023-02-09 11:25:57 +0100

    add handlers. TODO: check work or not

---
+++ app/handler/news.go
@@ -0,0 +1,73 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+	"yukonpr/models"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+func AddNews(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	news := models.NewsModel{}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&news); err != nil {
+		respondError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if err := db.Save(&news).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondJSON(w, http.StatusCreated, news)
+}
+func GetAllNews(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	news := []models.NewsModel{}
+	db.Find(&news)
+	respondJSON(w, http.StatusOK, news)
+}
+
+func GetNews(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	title := vars["Title"]
+	project := getNewsOr404(db, title, w, r)
+	if project == nil {
+		return
+	}
+	respondJSON(w, http.StatusOK, project)
+}
+
+func getNewsOr404(db *gorm.DB, title string, w http.ResponseWriter, r *http.Request) *models.NewsModel {
+	news := models.NewsModel{}
+	if err := db.First(&news, models.NewsModel{Title: title}).Error; err != nil {
+		respondError(w, http.StatusNotFound, err.Error())
+		return nil
+	}
+	return &news
+}
+
+// TODO: Check work it or not
+func GetNewsByTags(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	var news []models.NewsModel
+	var tags []models.Tags
+
+	tagNames := r.URL.Query()["tag"]
+
+	// Query the database for all news with the given tags
+	db.Where("name IN (?)", tagNames).Find(&tags)
+
+	// Get all news with the given tags
+	for _, tag := range tags {
+		db.Where("id = ?", tag.NewsId).Find(&news)
+	}
+
+	respondJSON(w, http.StatusOK, news)
+}
+func GetNewsBetweenTimes(from time.Time, to time.Time) ([]models.NewsModel, error)
+func GetNewsContainsWord(word string) ([]models.NewsModel, error)
---
+++ app/handler/tags.go
@@ -0,0 +1,79 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"yukonpr/models"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+func GetAllTags(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	newsTitle := vars["Title"]
+	project := getNewsOr404(db, newsTitle, w, r)
+	if project == nil {
+		return
+	}
+
+	tags := []models.Tags{}
+	if err := db.Model(&project).Related(&tags).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondJSON(w, http.StatusOK, tags)
+
+}
+func CreateTags(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	newsTitle := vars["title"]
+	news := getNewsOr404(db, newsTitle, w, r)
+	if news == nil {
+		return
+	}
+
+	tag := models.Tags{NewsId: int(news.ID)}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&tag); err != nil {
+		respondError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if err := db.Save(&tag).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondJSON(w, http.StatusCreated, tag)
+}
+
+func GetTask(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	newsTitle := vars["title"]
+	news := getNewsOr404(db, newsTitle, w, r)
+	if news == nil {
+		return
+	}
+
+	id, _ := strconv.Atoi(vars["id"])
+	tag := getTagsOr404(db, id, w, r)
+	if tag == nil {
+		return
+	}
+	respondJSON(w, http.StatusOK, tag)
+}
+
+func getTagsOr404(db *gorm.DB, id int, w http.ResponseWriter, r *http.Request) *models.Tags {
+	tags := models.Tags{}
+	if err := db.First(&tags, id).Error; err != nil {
+		respondError(w, http.StatusNotFound, err.Error())
+		return nil
+	}
+	return &tags
+}
