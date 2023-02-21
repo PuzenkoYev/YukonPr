7822109c3b3330b6814ee21eb92ed07d0ff70f63
PuzenkoYev <yevhenpuzenko@gmail.com>
2023-02-21 18:57:56 +0100

    Add rss xml scrapping

---
+++ app/scrapping/rssNewsModel.go
@@ -0,0 +1,75 @@
+package scrapping
+
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"time"
+	"yukonpr/app/models"
+)
+
+type Channel struct {
+	Title string `xml:"title"`
+	Link  string `xml:"link"`
+	Desc  string `xml:"description"`
+	Items []News `xml:"item"`
+}
+type News struct {
+	Title        string    `xml:"title"`
+	Description  string    `xml:"description"`
+	Image        Enclosure `xml:"enclosure"`
+	PubDate      string    `xml:"pubDate"`
+	FullNewsLink string    `xml:"link"`
+}
+type Enclosure struct {
+	Url    string `xml:"url,attr"`
+	Length int64  `xml:"length,attr"`
+	Type   string `xml:"type,attr"`
+}
+
+type Rss struct {
+	Channel Channel `xml:"channel"`
+}
+
+func (n News) Stringer() {
+	fmt.Println("Title:", n.Title)
+	fmt.Println("Description:", n.Description)
+	fmt.Println("ImageLink:", n.Image.Url)
+	fmt.Println("PubDate:", n.PubDate)
+	fmt.Println("FullNewsLink:", n.FullNewsLink)
+}
+func ParseRss(url string) Rss {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error GET: %v\n", err)
+		return Rss{}
+	}
+	defer resp.Body.Close()
+
+	rss := Rss{}
+
+	decoder := xml.NewDecoder(resp.Body)
+	err = decoder.Decode(&rss)
+	if err != nil {
+		fmt.Printf("Error Decode: %v\n", err)
+		return Rss{}
+	}
+
+	return rss
+}
+func ToNewsModel(oldModel News) models.NewsModel {
+	var newModel models.NewsModel
+	layout := "Mon, 02 Jan 2006 15:04:05 -0700"
+
+	newModel.Title = oldModel.Title
+	newModel.Image = oldModel.Image.Url
+	newModel.Text = oldModel.Description
+
+	t, err := time.Parse(layout, oldModel.PubDate)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	newModel.Time = t
+	return newModel
+}
